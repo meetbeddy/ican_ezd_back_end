7953ccf940b383c2bdcc31b576a7b38dc17a6bab
obed okpala <katlybedrick@gmail.com>
2022-02-19 23:23:33 +0100

    boring changes

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,43 +1,48 @@
 const mongoose = require("mongoose");
 const User = require("../models/User");
 const moment = require("moment");
+const env = require("dotenv");
+env.config();
 
 const connect = async () => {
-    try {
-        mongoose.set('useCreateIndex', true)
-        await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-        User.findOne({ email: "admin@admin.com" }).then(async user => {
-            if (!user) {
-                try {
-                    const admin = new User({
-                        role: { name: "Admin" },
-                        name: "admin",
-                        email: "admin@admin.com",
-                        password: "admin-secret",
-                        bankName: "Diamond Access",
-                        tellerNumber: "238987",
-                        tellerDate: moment(),
-                        phone: "08137173400",
-                        gender: "male",
-                        tshirtSize: "S",
-                        memberStatus: "ICAN Member",
-                        icanCode: "MB787654",
-                        memberCategory: "admin",
-                        memberAcronym: "ACA",
-                        nameOfSociety: "ABA DISTRICT",
-                        venue: "Physical"
-                    });
-                    await admin.save();
-                    console.log("Admin Created Succesfully")
-                } catch (e) {
-                    console.log(e.message)
-                    console.log(e)
-                }
-            }
-        });
-    } catch (e) {
-    console.log("🚀 ~ file: db_connection.js ~ line 39 ~ connect ~ e", e)
-    }
-}
+  try {
+    mongoose.set("useCreateIndex", true);
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    User.findOne({ email: "admin@admin.com" }).then(async (user) => {
+      if (!user) {
+        try {
+          const admin = new User({
+            role: { name: "Admin" },
+            name: "admin",
+            email: "admin@admin.com",
+            password: "admin-secret",
+            bankName: "Diamond Access",
+            tellerNumber: "238987",
+            tellerDate: moment(),
+            phone: "08137173400",
+            gender: "male",
+            tshirtSize: "S",
+            memberStatus: "ICAN Member",
+            icanCode: "MB787654",
+            memberCategory: "admin",
+            memberAcronym: "ACA",
+            nameOfSociety: "ABA DISTRICT",
+            venue: "Physical",
+          });
+          await admin.save();
+          console.log("Admin Created Succesfully");
+        } catch (e) {
+          console.log(e.message);
+          console.log(e);
+        }
+      }
+    });
+  } catch (e) {
+    console.log("🚀 ~ file: db_connection.js ~ line 39 ~ connect ~ e", e);
+  }
+};
 
 module.exports = connect;
--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const passport = require("passport");
-const cors = require('cors');
+const cors = require("cors");
 const connect_db = require("./config/db_connection");
-
 
 const authRoute = require("./routes/v1/auth");
 const AdminRoute = require("./routes/v1/admin");
 const userRoute = require("./routes/v1/user");
 const api = require("./routes/v1/angels");
+const env = require("dotenv");
+env.config();
 
-dotenv.config();
 const app = express();
-connect_db();
-app.use(bodyParser.json({ limit: "50mb"}));
+// connect_db();
+app.use(bodyParser.json({ limit: "50mb" }));
 // app.use(express.bodyParser({limit: '50mb'}));
 app.use(passport.initialize());
 require("./config/passport")(passport);
-app.use(cors())
+app.use(cors());
 app.use("/api/user", authRoute);
 app.use("/api/admin", AdminRoute);
 app.use("/api/attendant", userRoute);
 app.use("/api", api);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 // mongoose.set('useCreateIndex', true)
-// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+//database connect
+const db = process.env.DB_URI;
+
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("db connected"))
+  .catch((err) => console.log(err));
 app.listen(port, () => console.log("App Running on Port " + port));
